aa23dfe6b1ef3a53f8f06f8303ec9ce5cda03334
Jason Stallings <jason@stallin.gs>
2018-06-20 22:19:28 -0500

    More fixes.

--- renderer.js
+++ renderer.js
@@ -1,7 +1,8 @@
+const tld = require("tldjs");
+const remote = require('electron').remote;
+const win = remote.getCurrentWindow();
 $(function()
 {
-  const remote = require('electron').remote;
-  const win = remote.getCurrentWindow();
   //Close button.
   $("#closeButton").on("mousedown", function()
   {
@@ -58,7 +59,7 @@ $(function()
         search = "https://www.google.com/search?q=" + search;
       }
 
-      $("#view").attr("src", search);
+      $("#view").attr('src', search);
     }
   });
 
@@ -71,7 +72,7 @@ $(function()
     }
   });
 
-  //Leave info screen if background is clicked.
+  // Leave info screen if background is clicked.
   $("#info").on("mousedown", function(e)
   {
     if (e.target.id === "info")
@@ -81,49 +82,42 @@ $(function()
   });
 });
 
-
-win.on("document-end", function(frame)
+document.querySelector('webview').addEventListener("dom-ready", function()
 {
-  if (frame && frame.hasOwnProperty("id") && frame.id === "view")
-  {
-    setTimeout(function()
-    {
-      getBarColor();
-    },100);
-  }
+  getBarColor();
 });
 
 function getBarColor()
 {
-  win.capturePage(function(img)
+  document.querySelector('webview').capturePage(function(img)
   {
     var canvas = document.getElementById("color").getContext("2d");
     var image = document.getElementById("colorImage");
+    image.src = img.toDataURL();
 
-    image.src = img;
+    image.onload = () => {
+      // Draw the image to our invisible canvas.
+      canvas.drawImage(image,0,0);
 
-    //Draw the image to our invisible canvas.
-    canvas.drawImage(image,0,0);
+      //Get the pixel color right below the titlebar.
+      var pixelData = canvas.getImageData(0, 0, 1, 1).data;
+      //Change the title bar background color to the color found above.
+      document.getElementById("bar").style.backgroundColor = 'rgb(' + [pixelData[0],pixelData[1],pixelData[2]].join(',') + ')';
 
-    //Get the pixel color right below the titlebar.
-    var pixelData = canvas.getImageData(5, 35, 1, 1).data;
+      //Get the color the title bar text should be based on the title bar background. Currently black or white.
+      var inv = idealTextColor({R: pixelData[0], G: pixelData[1], B: pixelData[2]});
 
-    //Change the title bar background color to the color found above.
-    document.getElementById("bar").style.backgroundColor = 'rgb(' + [pixelData[0],pixelData[1],pixelData[2]].join(',') + ')';
+      //Set the title bar text.
+      document.getElementById("title").innerHTML = document.querySelector('webview').getTitle();
 
-    //Get the color the title bar text should be based on the title bar background. Currently black or white.
-    var inv = idealTextColor({R: pixelData[0], G: pixelData[1], B: pixelData[2]});
+      //Set the title bar text color.
+      document.getElementById("title").style.color = inv;
 
-    //Set the title bar text.
-    document.getElementById("title").innerHTML = document.getElementById("view").contentDocument.title;
+      // Set the address bar url to the current url.
+      $("#address").val(document.querySelector('webview').getURL());
 
-    //Set the title bar text color.
-    document.getElementById("title").style.color = inv;
-
-    //Set the address bar url to the current url.
-    $("#address").val($("#view")[0].contentWindow.location.href);
-
-  }, "png");
+    }
+  });
 }
 
 function idealTextColor(components)
